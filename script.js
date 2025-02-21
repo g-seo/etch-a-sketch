3898d2db5441f9d57e2bd2afba3f91eae07de203
Jiseo <jiseo.jake@gmail.com>
2025-02-21 22:59:10 +0900

    Make it to able to change size

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
-// 1. Intial setting(16x16 grid square)
-const monitor = document.querySelector('.monitor');
-for (let i = 1; i <= 16; i++) {
-    const div = document.createElement('div');
-    div.classList.add('container',`con_${i}`);
-    monitor.appendChild(div);
-}
-for (let i = 1; i <= 16; i++) {
-    const cont = document.querySelector(`.con_${i}`);
-    for (let j = 0; j < 16; j++) {
-        const div = document.createElement('div');
-        div.classList.add('block');
-        cont.appendChild(div);
+// 1. Function to create grid square
+function createGridSquare(number) {
+    const monitor = document.querySelector('.monitor');
+    // clear for making new grid square
+    while (monitor.firstChild) {
+        monitor.removeChild(monitor.firstChild);
     }
+    for (let i = 1; i <= number; i++) {
+        const div = document.createElement('div');
+        div.classList.add('container',`con_${i}`);
+        monitor.appendChild(div);
+    }
+    for (let i = 1; i <= number; i++) {
+        const cont = document.querySelector(`.con_${i}`);
+        for (let j = 0; j < number; j++) {
+            const div = document.createElement('div');
+            div.classList.add('block');
+            cont.appendChild(div);
+        }
+    }
+    // 2. Hovering effect
+    const block = document.querySelectorAll('.block');
+    block.forEach(function(e) {
+        e.addEventListener('mouseover', () => {
+            e.style['background-color'] = 'blue';
+        })
+        e.addEventListener('mouseout', () => {
+            e.style['background-color'] = 'white';
+        })
+    })
 }
 
-// 2. Hovering effect
-const block = document.querySelectorAll('.block');
-block.forEach(function(e) {
-    e.addEventListener('mouseover', () => {
-        e.style['background-color'] = 'blue';
-    })
-    e.addEventListener('mouseout', () => {
-        e.style['background-color'] = 'white';
-    })
-})
+// 3. Button to change size
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', () => {
+    let num = prompt('Type positive integer smaller than 100 for size');
+    createGridSquare(num);
+});
+
+// Initial 16x16 grid square
+createGridSquare(16);
